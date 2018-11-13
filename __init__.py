7b64589c1ed295f8f0961c77d0e23221f3359801
philc <philc@arcx.com>
2018-11-13 18:12:49 -0500

    Added Delay On / Off Option

--- __init__.py
+++ __init__.py
@@ -105,6 +105,8 @@ class DecoraWifiSkill(MycroftSkill):
 
     def handle_decora_light_on_intent(self, message):
         silent_kw = message.data.get("SilentKeyword")
+        delay_kw = message.data.get("DelayKeyword")
+        LOG.info(delay_kw)
         my_switch = self.get_switch_id()
         my_switch.update_attributes({'power': 'ON', 'brightness': '100'})
         if silent_kw:
@@ -114,6 +116,8 @@ class DecoraWifiSkill(MycroftSkill):
 
     def handle_decora_light_off_intent(self, message):
         silent_kw = message.data.get("SilentKeyword")
+        delay_kw = message.data.get("DelayKeyword")
+        LOG.info(delay_kw)
         my_switch = self.get_switch_id()
         my_switch.update_attributes({'power': 'OFF'})
         if silent_kw:
